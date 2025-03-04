e4ead2c30228b57858167362f639f20e9069148f
HuuLoc <huuloc@192.168.1.13.non-exists.ptr.local>
2025-03-04 16:40:14 +0700

    update refresh

--- script.js
+++ script.js
@@ -1,51 +1,96 @@
-(function () {
-  var _0x4b5d = [
-    "addEventListener",
-    "getElementById",
-    "contactForm",
-    "submit",
-    "preventDefault",
-    "value",
-    "name",
-    "email",
-    "message",
-    "entry.2005620554",
-    "entry.802662662",
-    "entry.1422570857",
-    "fetch",
-    "POST",
-    "no-cors",
-    "Gửi thành công!",
-    "reset",
-    "Lỗi gửi form:",
-    "Gửi thất bại, vui lòng thử lại!",
-  ];
-  document[_0x4b5d[1]](_0x4b5d[2])[_0x4b5d[0]](
-    _0x4b5d[3],
-    function (_0x1a4e8e) {
-      _0x1a4e8e[_0x4b5d[4]]();
-      var _0x48b892 = document[_0x4b5d[1]](_0x4b5d[5])[_0x4b5d[6]];
-      var _0x4eab5b = document[_0x4b5d[1]](_0x4b5d[7])[_0x4b5d[6]];
-      var _0x1e8c17 = document[_0x4b5d[1]](_0x4b5d[8])[_0x4b5d[6]];
-      var _0x252e10 =
-        "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfxWZY838bCEA_JPzkYopbnKe-JKHHnZfJ2O1uZn-ehz8doxw/formResponse";
-      var _0x2e58c5 = new URLSearchParams();
-      _0x2e58c5.append(_0x4b5d[9], _0x48b892);
-      _0x2e58c5.append(_0x4b5d[10], _0x1e8c17);
-      _0x2e58c5.append(_0x4b5d[11], _0x4eab5b);
-      fetch(_0x252e10, {
-        method: _0x4b5d[12],
-        mode: _0x4b5d[13],
-        body: _0x2e58c5,
-      })
-        .then(() => {
-          alert(_0x4b5d[14]);
-          document[_0x4b5d[1]](_0x4b5d[2])[_0x4b5d[15]]();
+function _0x3747(_0x339259, _0x21de87) {
+  const _0x109b44 = _0x109b();
+  return (
+    (_0x3747 = function (_0x374743, _0x2a4adc) {
+      _0x374743 = _0x374743 - 0xba;
+      let _0x26c90c = _0x109b44[_0x374743];
+      return _0x26c90c;
+    }),
+    _0x3747(_0x339259, _0x21de87)
+  );
+}
+const _0x240b6e = _0x3747;
+(function (_0x4932cb, _0x20cc03) {
+  const _0x52beff = _0x3747,
+    _0x521201 = _0x4932cb();
+  while (!![]) {
+    try {
+      const _0x4782ef =
+        parseInt(_0x52beff(0xca)) / 0x1 +
+        (parseInt(_0x52beff(0xc8)) / 0x2) * (-parseInt(_0x52beff(0xc5)) / 0x3) +
+        parseInt(_0x52beff(0xd0)) / 0x4 +
+        -parseInt(_0x52beff(0xce)) / 0x5 +
+        parseInt(_0x52beff(0xc9)) / 0x6 +
+        (parseInt(_0x52beff(0xd1)) / 0x7) * (-parseInt(_0x52beff(0xc1)) / 0x8) +
+        parseInt(_0x52beff(0xbd)) / 0x9;
+      if (_0x4782ef === _0x20cc03) break;
+      else _0x521201["push"](_0x521201["shift"]());
+    } catch (_0x9b7c8a) {
+      _0x521201["push"](_0x521201["shift"]());
+    }
+  }
+})(_0x109b, 0x400c9),
+  document[_0x240b6e(0xd2)](_0x240b6e(0xcf))[_0x240b6e(0xcc)](
+    _0x240b6e(0xd3),
+    function (_0x192b74) {
+      const _0x92bc7a = _0x240b6e;
+      _0x192b74[_0x92bc7a(0xbe)]();
+      const _0x58ce96 = document[_0x92bc7a(0xd2)]("name")["value"],
+        _0x449d1b = document[_0x92bc7a(0xd2)](_0x92bc7a(0xbc))[_0x92bc7a(0xcd)],
+        _0x21a3d8 = document[_0x92bc7a(0xd2)](_0x92bc7a(0xba))[_0x92bc7a(0xcd)],
+        _0x3405d6 = _0x92bc7a(0xc3),
+        _0x3069cc = new URLSearchParams();
+      _0x3069cc["append"]("entry.2005620554", _0x58ce96),
+        _0x3069cc[_0x92bc7a(0xc0)](_0x92bc7a(0xc4), _0x21a3d8),
+        _0x3069cc["append"]("entry.1422570857", _0x449d1b),
+        fetch(_0x3405d6, {
+          method: _0x92bc7a(0xbf),
+          mode: _0x92bc7a(0xc2),
+          body: _0x3069cc,
         })
-        .catch((_0x3b62b4) => {
-          console.error(_0x4b5d[16], _0x3b62b4);
-          alert(_0x4b5d[17]);
-        });
+          [_0x92bc7a(0xcb)](() => {
+            const _0x38d1d1 = _0x92bc7a;
+            alert("Gửi\x20thành\x20công!"),
+              document[_0x38d1d1(0xd2)](_0x38d1d1(0xcf))[_0x38d1d1(0xc6)]();
+          })
+          ["catch"]((_0x38785d) => {
+            const _0x357ef7 = _0x92bc7a;
+            console[_0x357ef7(0xc7)](_0x357ef7(0xbb), _0x38785d),
+              alert("Gửi\x20thất\x20bại,\x20vui\x20lòng\x20thử\x20lại!");
+          });
     }
   );
-})();
+function _0x109b() {
+  const _0x53a666 = [
+    "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfxWZY838bCEA_JPzkYopbnKe-JKHHnZfJ2O1uZn-ehz8doxw/formResponse",
+    "entry.802662662",
+    "201tKHgdi",
+    "reset",
+    "error",
+    "10202vQWiHn",
+    "624270hdoLYA",
+    "122406CIlXLp",
+    "then",
+    "addEventListener",
+    "value",
+    "1047035eUkaAu",
+    "contactForm",
+    "677276fSuykI",
+    "133LwUcvn",
+    "getElementById",
+    "submit",
+    "message",
+    "Lỗi\x20gửi\x20form:",
+    "email",
+    "8418636fndenQ",
+    "preventDefault",
+    "POST",
+    "append",
+    "217960rGPEpw",
+    "no-cors",
+  ];
+  _0x109b = function () {
+    return _0x53a666;
+  };
+  return _0x109b();
+}
